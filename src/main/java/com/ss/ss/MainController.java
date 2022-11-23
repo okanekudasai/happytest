e767e5f3820e603ed600e4fe779a7e34a29bb1d2
abcd <ss@ss.ss>
2022-11-23 14:55:36 +0900

    2

--- src/main/java/com/ss/ss/MainController.java
+++ src/main/java/com/ss/ss/MainController.java
@@ -3,9 +3,11 @@ package com.ss.ss;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class MainController {
 	@Autowired
